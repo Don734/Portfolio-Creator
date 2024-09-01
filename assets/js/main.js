857cc286ef757b3bbe23f27cc41a8f9c8800444e
Doniyor Rakhimov <d.rakhimov@ginza.global>
2024-09-01 15:26:21 +0500

    portfolio upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     mobileMenu();
     projectSlider();
+    testimonialSlider();
 })
 
+function mobileMenu() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu = document.querySelector('.navbar');
+
+    menuBtn.addEventListener('click', (e) => {
+        if (menuBtn.classList.contains('active')) {
+            menuBtn.classList.remove('active');
+        } else {
+            menuBtn.classList.add('active');
+        }
+        
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+        } else {
+            menu.classList.add('active');
+        }
+    });
+}
+
 function projectSlider() {
-    let swiperContainer = '.swiper-projects';
-    let swiperOptions = {
+    const swiperContainer = '.swiper-projects';
+    const swiperOptions = {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -28,21 +48,29 @@ function projectSlider() {
     const swiper = new Swiper(swiperContainer, swiperOptions);
 }
 
-function mobileMenu() {
-    const menuBtn = document.querySelector('.menu-btn');
-    const menu = document.querySelector('.navbar');
-
-    menuBtn.addEventListener('click', (e) => {
-        if (menuBtn.classList.contains('active')) {
-            menuBtn.classList.remove('active');
-        } else {
-            menuBtn.classList.add('active');
-        }
-        
-        if (menu.classList.contains('active')) {
-            menu.classList.remove('active');
-        } else {
-            menu.classList.add('active');
-        }
-    });
+function testimonialSlider() {
+    const swiperContainerImage = '.swiper-image';
+    const swiperContainerQuote = '.swiper-quote';
+    const swiperOptionsImages = {
+        effect: 'flip',
+        loop: false, // Not recommended to enable!!!
+        slidesPerView: 1,
+        spaceBetween: 20,
+    };
+    const swiperOptionsQuote = {
+        direction: "vertical",
+        effect: "slide",
+        loop: false, // Not recommended to enable!!!
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+    };
+    const swiperImages = new Swiper(swiperContainerImage, swiperOptionsImages);
+    const swiper = new Swiper(swiperContainerQuote, swiperOptionsQuote);
+    swiper.controller.control = swiperImages;
+    swiperImages.controller.control = swiper;
 }
